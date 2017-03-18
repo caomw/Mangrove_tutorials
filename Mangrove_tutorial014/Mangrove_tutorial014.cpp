4677835ff138499749c0ad00f903b09dbb3a7470
David Canino <canino.david@gmail.com>
2017-03-18 14:37:04 +0100

    Minor revisions and updates to several tutorials

--- Mangrove_tutorial014/Mangrove_tutorial014.cpp
+++ Mangrove_tutorial014/Mangrove_tutorial014.cpp
@@ -18,7 +18,7 @@
  *
  * Mangrove_tutorial014.cpp - the 'Tutorial Example-014' for the 'Mangrove TDS Library 3.0'.
  *************************************************************************************************************************************************************************/
- 
+
 #include "Mangrove_Miscellanea.h"
 #include <cstdlib>
 #include <iostream>
@@ -26,17 +26,17 @@
 using namespace std;
 using namespace mangrove_tds;
 
-/// The <i>main function</i> for the <i>Mangrove_tutorial014</i> program.
+/// The <i>main function</i> for the <i>'Mangrove_tutorial014'</i> program.
 /**
  * \file Mangrove_tutorial014.cpp
  * \author <A href="http://davidcanino.github.io">David Canino</A> (e-mail: <A href="mailto:canino.david@gmail.com">David Canino</A>)
  */
 
-/// The <i>main function</i> for the <i>Mangrove_tutorial014</i> program.
+/// The <i>main function</i> for the <i>'Mangrove_tutorial014'</i> program.
 /**
  * \return <ul><li>the built-in <i>EXIT_SUCCESS</i> constant value (provided by the <i><A href="http://en.wikipedia.org/wiki/C%2B%2B_Standard_Library">Standard C++ Library</A></i>), if all is ok</li><li>the built-in <i>EXIT_FAILURE</i> constant value (provided by 
  * the <i><A href="http://en.wikipedia.org/wiki/C%2B%2B_Standard_Library">Standard C++ Library</A></i>), otherwise</li></ul>
- * \see mangrove_tds::Mangrove_exportCopyrightDisclaimer()
+ * \see mangrove_tds::Mangrove_exportCopyrightDisclaimer(), mangrove_tds::Mangrove_pause()
  */
 int main(void)
 {
@@ -46,9 +46,12 @@ int main(void)
 	ofstream out;
 	ifstream in;
 
-	/* Now, we validate several functions in the 'Mangrove_Miscellanea.h' file, focused for manipulating the pairs, involving a boolean flag and arrays/deques/lists of several template values (here, specialized for the 'uint' values). */
-	Mangrove_exportCopyrightDisclaimer("Mangrove_tutorial014");
-	cout<<"\tConstructing a pair 'p=(b,d)', involving a boolean flag 'b' and a deque 'd' of several 'uint' values ... ";
+	/* This is the 'Mangrove_tutorial014' tutorial, where the pairs, involving a boolean flag and arrays/deques/lists of several template values (here, specialized for the 'uint' C++ built-in values), are validated. */
+	Mangrove_exportCopyrightDisclaimer ("The 'Mangrove_tutorial014' Tutorial");
+	cout.flush();
+	
+	/* TASK #1 - validating the pairs, involving a boolean flag and a deque of 'uint' C++ built-in values. */
+	cout<<"\tConstructing a new pair 'p=(b,d)', involving a boolean flag 'b' and a deque 'd' of several 'uint' C++ built-in values ... ";
 	pd0.first=true;
 	pd0.second=deque<uint>();
 	for(uint k=0;k<5;k++) pd0.second.push_back(k);
@@ -77,8 +80,8 @@ int main(void)
 	pd0.second.clear();
 	Mangrove_pause();
 	
-	/* Now, we validate the pairs, involving lists of 'uint' values. */
-	cout<<"\tConstructing a pair 'p=(b,l)', involving a boolean flag 'b' and a list 'l' of several 'uint' values ... ";
+	/* TASK #2 - validating the pairs, involving a boolean flag and a list of 'uint' C++ built-in values. */
+	cout<<"\tConstructing a new pair 'p=(b,l)', involving a boolean flag 'b' and a list 'l' of several 'uint' C++ built-in values ... ";
 	pl0.first=false;
 	pl0.second=list<uint>();
 	for(uint k=0;k<5;k++) pl0.second.push_back(3*k);
@@ -106,9 +109,9 @@ int main(void)
 	pl0.first=false;
 	pl0.second.clear();
 	Mangrove_pause();
-	
-	/* Now, we validate the pairs, involving arrays of 'uint' values. */
-	cout<<"\tConstructing a pair 'p=(b,a)', involving a boolean flag 'b' and an array 'a' of several 'uint' values ... ";
+
+	/* TASK #3 - validating the pairs, involving a boolean flag and an array of 'uint' C++ built-in values. */
+	cout<<"\tConstructing a new pair 'p=(b,a)', involving a boolean flag 'b' and an array 'a' of several 'uint' C++ built-in values ... ";
 	pa0.first=false;
 	pa0.second=vector<uint>();
 	pa0.second.push_back(8);
@@ -135,5 +138,13 @@ int main(void)
 	cout<<"ok"<<endl<<endl;
 	pa0.first=false;
 	pa0.second.clear();
+
+	/* TASK #4 - deallocating all pairs of in this tutorial. */
+	cout<<"\tDeallocating the content of all pairs in this tutorial ... ";
+	pd0.second.clear();
+	pl0.second.clear();
+	pa0.second.clear();
+	cout<<"ok"<<endl<<endl;
+	cout.flush();
 	return EXIT_SUCCESS;
 }
