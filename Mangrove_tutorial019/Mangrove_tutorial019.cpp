c548af6a87b17e7b1d88c885a57359767333b7c9
David Canino <canino.david@gmail.com>
2017-03-18 14:35:22 +0100

    Minor revisions and updates to several tutorials

--- Mangrove_tutorial019/Mangrove_tutorial019.cpp
+++ Mangrove_tutorial019/Mangrove_tutorial019.cpp
@@ -18,7 +18,7 @@
  *
  * Mangrove_tutorial019.cpp - the 'Tutorial Example-019' for the 'Mangrove TDS Library 3.0'.
  *************************************************************************************************************************************************************************/
- 
+
 #include "Mangrove_Miscellanea.h"
 #include <cstdlib>
 #include <iostream>
@@ -26,17 +26,17 @@
 using namespace std;
 using namespace mangrove_tds;
 
-/// The <i>main function</i> for the <i>Mangrove_tutorial019</i> program.
+/// The <i>main function</i> for the <i>'Mangrove_tutorial019'</i> program.
 /**
  * \file Mangrove_tutorial019.cpp
  * \author <A href="http://davidcanino.github.io">David Canino</A> (e-mail: <A href="mailto:canino.david@gmail.com">David Canino</A>)
  */
 
-/// The <i>main function</i> for the <i>Mangrove_tutorial019</i> program.
+/// The <i>main function</i> for the <i>'Mangrove_tutorial019'</i> program.
 /**
  * \return <ul><li>the built-in <i>EXIT_SUCCESS</i> constant value (provided by the <i><A href="http://en.wikipedia.org/wiki/C%2B%2B_Standard_Library">Standard C++ Library</A></i>), if all is ok</li><li>the built-in <i>EXIT_FAILURE</i> constant value (provided by 
  * the <i><A href="http://en.wikipedia.org/wiki/C%2B%2B_Standard_Library">Standard C++ Library</A></i>), otherwise</li></ul>
- * \see mangrove_tds::Mangrove_exportCopyrightDisclaimer()
+ * \see mangrove_tds::Mangrove_exportCopyrightDisclaimer(), mangrove_tds::Mangrove_pause()
  */
 int main(void)
 {
@@ -44,20 +44,18 @@ int main(void)
 	vector< set<uint> > as;
 	ofstream out;
 	ifstream in;
-	
-	/* Now, we validate several functions in the 'Mangrove_Miscellanea.h' file, focused for manipulating the nested arrays, containing several arrays of 'uint' values. */
-	Mangrove_exportCopyrightDisclaimer("Mangrove_tutorial019");
-	cout<<"\tCreating the nested array 'a', containing several arrays of 'uint' values, ... ";
-	a.push_back( vector<uint>() );
-	a.back().push_back(4);
-	a.back().push_back(2);
-	a.back().push_back(5);
-	a.push_back( vector<uint>() );
-	for(uint k=10;k>6;k--) a.back().push_back(k);
-	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the nested array 'a': "<<Mangrove_valuesNumber(a)<<endl;
+
+	/* This is the 'Mangrove_tutorial019' tutorial, where the nested arrays of several 'uint' C++ built-in values are validated. */
+	Mangrove_exportCopyrightDisclaimer ("The 'Mangrove_tutorial019' Tutorial");
 	cout.flush();
+	
+	/* TASK #1 - creating a new nested array, containing several arrays of 'uint' C++ built-in values. */
+	cout<<"\tCreating the nested array 'a', containing several arrays of 'uint' C++ built-in values, ... ";
+	a.push_back( vector<uint>() );
+	a.back()={10,20,30,40};
+	cout<<"ok"<<endl;
 	Mangrove_debug(a);
+	cout<<"\tThe total number of the 'uint' C++ built-in values in the nested array 'a' is "<<Mangrove_valuesNumber(a)<<"."<<endl;
 	cout<<"\tExporting the 'compact' representation of the nested array 'a' on the 'nested_array_compact.txt' file ... ";
 	out.open("nested_array_compact.txt");
 	out<<a;
@@ -67,29 +65,33 @@ int main(void)
 	in.open("nested_array_compact.txt");
 	in>>a1;
 	in.close();
-	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the reconstructed nested array 'a': "<<Mangrove_valuesNumber(a1)<<endl;
+	cout<<"ok"<<endl;
 	Mangrove_debug(a1);
-	if(Mangrove_theSame(a,a1)) cout<<"\tThe content of the reconstructed nested array coincides with the content of the original nested array 'a'"<<endl<<endl;
-	else cout<<"\tThe content of the reconstructed nested array does not coincide with the content of the original nested array 'a'"<<endl<<endl;
+	cout<<"\tThe total number of the 'uint' C++ built-in values in the reconstructed nested array 'a' is "<<Mangrove_valuesNumber(a1)<<"."<<endl;
+	if(Mangrove_theSame(a,a1)) cout<<"\tThe content of the reconstructed nested array coincides with the content of the original nested array 'a'."<<endl<<endl;
+	else cout<<"\tThe content of the reconstructed nested array does not coincide with the content of the original nested array 'a'."<<endl<<endl;
 	cout<<"\tRemoving the 'nested_array_compact.txt' file ... ";
 	Mangrove_removeFile(string("nested_array_compact.txt"));
 	cout<<"ok"<<endl<<endl;
 	Mangrove_destroy(a1);
 	Mangrove_pause();
 	
-	/* Now, we validate the functions for manipulating the nested arrays, containing several sets of 'uint' values. */
-	cout<<"\tConverting the nested array 'a' into the nested array 'as', containing several sets of 'uint' values, ... ";
+	/* TASK #2 - converting the nested array 'a' to the corresponding nested array of several sets. */
+	cout<<"\tConverting the nested array 'a' into the nested array 'as', containing several sets of 'uint' C++ built-in values, ... ";
 	Mangrove_convert(a,as);
 	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the new nested array 'as': "<<Mangrove_valuesNumber(as)<<endl;
+	cout<<"\tIMPORTANT:\tthe 'uint' C++ built-in values in the sets of interest are sorted with respect to the '<' and the '==' comparison operators."<<endl<<endl;
 	cout.flush();
 	Mangrove_debug(as);
-	cout<<"\tConverting the nested array 'as' into the corresponding nesting array, containing several arrays, ... ";
+	cout<<"\tThe total number of the 'uint' C++ built-in values in the new nested array 'as' is "<<Mangrove_valuesNumber(as)<<"."<<endl<<endl;
+	cout<<"\tConverting the nested array 'as' back to the corresponding nesting array, containing several arrays, ... ";
 	Mangrove_convert(as,a1);
-	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the new nested array: "<<Mangrove_valuesNumber(a1)<<endl;
-	Mangrove_debug(a1);
+	cout<<"ok"<<endl;
+	if(Mangrove_theSame(a,a1)) cout<<"\tThe content of the new nested array coincides with the content of the original nested array 'a'."<<endl<<endl;
+	else cout<<"\tThe content of the new nested array does not coincide with the content of the original nested array 'a'."<<endl<<endl;
+	cout.flush();
+	
+	/* TASK #3 - deallocating all nested arrays in this tutorial. */
 	cout<<"\tDeallocating all nested arrays of this tutorial ... ";
 	Mangrove_destroy(a);
 	Mangrove_destroy(a1);
