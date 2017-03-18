ea43180185fb653bda3a1ae8fce0615765bdaf34
David Canino <canino.david@gmail.com>
2017-03-18 14:38:52 +0100

    Minor revisions and updates to several tutorials

--- Mangrove_tutorial018/Mangrove_tutorial018.cpp
+++ Mangrove_tutorial018/Mangrove_tutorial018.cpp
@@ -18,7 +18,7 @@
  *
  * Mangrove_tutorial018.cpp - the 'Tutorial Example-018' for the 'Mangrove TDS Library 3.0'.
  *************************************************************************************************************************************************************************/
- 
+
 #include "Mangrove_Miscellanea.h"
 #include <cstdlib>
 #include <iostream>
@@ -26,17 +26,17 @@
 using namespace std;
 using namespace mangrove_tds;
 
-/// The <i>main function</i> for the <i>Mangrove_tutorial018</i> program.
+/// The <i>main function</i> for the <i>'Mangrove_tutorial018'</i> program.
 /**
  * \file Mangrove_tutorial018.cpp
  * \author <A href="http://davidcanino.github.io">David Canino</A> (e-mail: <A href="mailto:canino.david@gmail.com">David Canino</A>)
  */
 
-/// The <i>main function</i> for the <i>Mangrove_tutorial018</i> program.
+/// The <i>main function</i> for the <i>'Mangrove_tutorial018'</i> program.
 /**
  * \return <ul><li>the built-in <i>EXIT_SUCCESS</i> constant value (provided by the <i><A href="http://en.wikipedia.org/wiki/C%2B%2B_Standard_Library">Standard C++ Library</A></i>), if all is ok</li><li>the built-in <i>EXIT_FAILURE</i> constant value (provided by 
  * the <i><A href="http://en.wikipedia.org/wiki/C%2B%2B_Standard_Library">Standard C++ Library</A></i>), otherwise</li></ul>
- * \see mangrove_tds::Mangrove_exportCopyrightDisclaimer()
+ * \see mangrove_tds::Mangrove_exportCopyrightDisclaimer(), mangrove_tds::Mangrove_pause()
  */
 int main(void)
 {
@@ -44,19 +44,19 @@ int main(void)
 	list< set<uint> > ls;
 	ofstream out;
 	ifstream in;
-	
-	/* Now, we validate several functions in the 'Mangrove_Miscellanea.h' file, focused for manipulating the nested lists, containing several lists of 'uint' values. */
-	Mangrove_exportCopyrightDisclaimer("Mangrove_tutorial018");
-	cout<<"\tCreating the nested list 'l', containing several lists of 'uint' values, ... ";
-	l.push_back( list<uint>() );
-	l.push_back( list<uint>() );
-	l.back().push_back(4);
-	l.back().push_back(2);
-	l.back().push_back(5);
-	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the nested list 'l': "<<Mangrove_valuesNumber(l)<<endl;
+
+	/* This is the 'Mangrove_tutorial018' tutorial, where the nested lists of several 'uint' C++ built-in values are validated. */
+	Mangrove_exportCopyrightDisclaimer ("The 'Mangrove_tutorial018' Tutorial");
 	cout.flush();
+	
+	/* TASK #1 - creating a new nested list, containing several lists of 'uint' C++ built-in values. */
+	cout<<"\tCreating the nested list 'l', containing several lists of 'uint' C++ built-in values, ... ";
+	l.push_back( list<uint>() );
+	l.push_back( list<uint>() );
+	for(uint k=10;k>6;k--) l.back().push_back(5*k);
+	cout<<"ok"<<endl;
 	Mangrove_debug(l);
+	cout<<"\tThe total number of the 'uint' C++ built-in values in the nested list 'l' is "<<Mangrove_valuesNumber(l)<<"."<<endl;
 	cout<<"\tExporting the 'compact' representation of the nested list 'l' on the 'nested_list_compact.txt' file ... ";
 	out.open("nested_list_compact.txt");
 	out<<l;
@@ -66,29 +66,33 @@ int main(void)
 	in.open("nested_list_compact.txt");
 	in>>l1;
 	in.close();
-	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the reconstructed nested list 'l': "<<Mangrove_valuesNumber(l1)<<endl;
+	cout<<"ok"<<endl;
 	Mangrove_debug(l1);
-	if(Mangrove_theSame(l,l1)) cout<<"\tThe content of the reconstructed nested list coincides with the content of the original nested list 'l'"<<endl<<endl;
-	else cout<<"\tThe content of the reconstructed nested list does not coincide with the content of the original nested list 'l'"<<endl<<endl;
+	cout<<"\tThe total number of the 'uint' C++ built-in values in the reconstructed nested list 'l' is "<<Mangrove_valuesNumber(l1)<<"."<<endl;
+	if(Mangrove_theSame(l,l1)) cout<<"\tThe content of the reconstructed nested list coincides with the content of the original nested list 'l'."<<endl<<endl;
+	else cout<<"\tThe content of the reconstructed nested list does not coincide with the content of the original nested list 'l'."<<endl<<endl;
 	cout<<"\tRemoving the 'nested_list_compact.txt' file ... ";
 	Mangrove_removeFile(string("nested_list_compact.txt"));
 	cout<<"ok"<<endl<<endl;
 	Mangrove_destroy(l1);
 	Mangrove_pause();
 	
-	/* Now, we validate the functions for manipulating the nested lists, containing several sets of 'uint' values. */
-	cout<<"\tConverting the nested list 'l' into the nested list 'ls', containing several sets of 'uint' values, ... ";
+	/* TASK #2 - converting the nested list 'l' to the corresponding nested list of several sets. */
+	cout<<"\tConverting the nested list 'l' into the nested list 'ls', containing several sets of 'uint' C++ built-in values, ... ";
 	Mangrove_convert(l,ls);
 	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the new nested list 'ls': "<<Mangrove_valuesNumber(ls)<<endl;
+	cout<<"\tIMPORTANT:\tthe 'uint' C++ built-in values in the sets of interest are sorted with respect to the '<' and the '==' comparison operators."<<endl<<endl;
 	cout.flush();
 	Mangrove_debug(ls);
-	cout<<"\tConverting the nested list 'ls' into the corresponding nesting list, containing several lists, ... ";
+	cout<<"\tThe total number of the 'uint' C++ built-in values in the new nested list 'ls' is "<<Mangrove_valuesNumber(ls)<<"."<<endl<<endl;
+	cout<<"\tConverting the nested list 'ls' back to the corresponding nesting list, containing several lists, ... ";
 	Mangrove_convert(ls,l1);
-	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the new nested list: "<<Mangrove_valuesNumber(l1)<<endl;
-	Mangrove_debug(l1);
+	cout<<"ok"<<endl;
+	if(Mangrove_theSame(l,l1)) cout<<"\tThe content of the new nested list coincides with the content of the original nested list 'l'."<<endl<<endl;
+	else cout<<"\tThe content of the new nested list does not coincide with the content of the original nested list 'l'."<<endl<<endl;
+	cout.flush();
+	
+	/* TASK #3 - deallocating all nested lists in this tutorial. */
 	cout<<"\tDeallocating all nested lists of this tutorial ... ";
 	Mangrove_destroy(l);
 	Mangrove_destroy(l1);
