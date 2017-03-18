a09a77d2226dbd6b10b44adb1035a512f942666c
David Canino <canino.david@gmail.com>
2017-03-18 14:38:08 +0100

    Minor revisions and updates to several tutorials

--- Mangrove_tutorial017/Mangrove_tutorial017.cpp
+++ Mangrove_tutorial017/Mangrove_tutorial017.cpp
@@ -18,7 +18,7 @@
  *
  * Mangrove_tutorial017.cpp - the 'Tutorial Example-017' for the 'Mangrove TDS Library 3.0'.
  *************************************************************************************************************************************************************************/
- 
+
 #include "Mangrove_Miscellanea.h"
 #include <cstdlib>
 #include <iostream>
@@ -26,17 +26,17 @@
 using namespace std;
 using namespace mangrove_tds;
 
-/// The <i>main function</i> for the <i>Mangrove_tutorial017</i> program.
+/// The <i>main function</i> for the <i>'Mangrove_tutorial017'</i> program.
 /**
  * \file Mangrove_tutorial017.cpp
  * \author <A href="http://davidcanino.github.io">David Canino</A> (e-mail: <A href="mailto:canino.david@gmail.com">David Canino</A>)
  */
 
-/// The <i>main function</i> for the <i>Mangrove_tutorial017</i> program.
+/// The <i>main function</i> for the <i>'Mangrove_tutorial017'</i> program.
 /**
  * \return <ul><li>the built-in <i>EXIT_SUCCESS</i> constant value (provided by the <i><A href="http://en.wikipedia.org/wiki/C%2B%2B_Standard_Library">Standard C++ Library</A></i>), if all is ok</li><li>the built-in <i>EXIT_FAILURE</i> constant value (provided by 
  * the <i><A href="http://en.wikipedia.org/wiki/C%2B%2B_Standard_Library">Standard C++ Library</A></i>), otherwise</li></ul>
- * \see mangrove_tds::Mangrove_exportCopyrightDisclaimer()
+ * \see mangrove_tds::Mangrove_exportCopyrightDisclaimer(), mangrove_tds::Mangrove_pause()
  */
 int main(void)
 {
@@ -45,17 +45,19 @@ int main(void)
 	ofstream out;
 	ifstream in;
 
-	/* Now, we validate several functions in the 'Mangrove_Miscellanea.h' file, focused for manipulating the nested deques, containing several deques of 'uint' values. */
-	Mangrove_exportCopyrightDisclaimer("Mangrove_tutorial017");
-	cout<<"\tCreating the nested deque 'd', containing several deques of 'uint' values, ... ";
+	/* This is the 'Mangrove_tutorial017' tutorial, where the nested deques of several 'uint' C++ built-in values are validated. */
+	Mangrove_exportCopyrightDisclaimer ("The 'Mangrove_tutorial017' Tutorial");
+	cout.flush();
+	
+	/* TASK #1 - creating a new nested deque, containing several deques of 'uint' C++ built-in values. */
+	cout<<"\tCreating the nested deque 'd', containing several deques of 'uint' C++ built-in values, ... ";
 	d.push_back( deque<uint>() );
 	d.back().push_back(3);
 	d.push_back( deque<uint>() );
 	for(uint k=10;k>6;k--) d.back().push_back(2*k);
-	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the nested deque 'd': "<<Mangrove_valuesNumber(d)<<endl;
-	cout.flush();
+	cout<<"ok"<<endl;
 	Mangrove_debug(d);
+	cout<<"\tThe total number of the 'uint' C++ built-in values in the nested deque 'd' is "<<Mangrove_valuesNumber(d)<<"."<<endl;
 	cout<<"\tExporting the 'compact' representation of the nested deque 'd' on the 'nested_deque_compact.txt' file ... ";
 	out.open("nested_deque_compact.txt");
 	out<<d;
@@ -65,29 +67,33 @@ int main(void)
 	in.open("nested_deque_compact.txt");
 	in>>d1;
 	in.close();
-	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the reconstructed nested deque 'd': "<<Mangrove_valuesNumber(d1)<<endl;
+	cout<<"ok"<<endl;
 	Mangrove_debug(d1);
-	if(Mangrove_theSame(d,d1)) cout<<"\tThe content of the reconstructed nested deque coincides with the content of the original nested deque 'd'"<<endl<<endl;
-	else cout<<"\tThe content of the reconstructed nested deque does not coincide with the content of the original nested deque 'd'"<<endl<<endl;
+	cout<<"\tThe total number of the 'uint' C++ built-in values in the reconstructed nested deque 'd' is "<<Mangrove_valuesNumber(d1)<<"."<<endl;
+	if(Mangrove_theSame(d,d1)) cout<<"\tThe content of the reconstructed nested deque coincides with the content of the original nested deque 'd'."<<endl<<endl;
+	else cout<<"\tThe content of the reconstructed nested deque does not coincide with the content of the original nested deque 'd'."<<endl<<endl;
 	cout<<"\tRemoving the 'nested_deque_compact.txt' file ... ";
 	Mangrove_removeFile(string("nested_deque_compact.txt"));
 	cout<<"ok"<<endl<<endl;
 	Mangrove_destroy(d1);
 	Mangrove_pause();
 	
-	/* Now, we validate the functions for manipulating the nested deques, containing several sets of 'uint' values. */
-	cout<<"\tConverting the nested deque 'd' into the nested deque 'ds', containing several sets of 'uint' values, ... ";
+	/* TASK #2 - converting the nested deque 'd' to the corresponding nested deque of several sets. */
+	cout<<"\tConverting the nested deque 'd' into the nested deque 'ds', containing several sets of 'uint' C++ built-in values, ... ";
 	Mangrove_convert(d,ds);
 	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the new nested deque 'ds': "<<Mangrove_valuesNumber(ds)<<endl;
+	cout<<"\tIMPORTANT:\tthe 'uint' C++ built-in values in the sets of interest are sorted with respect to the '<' and the '==' comparison operators."<<endl<<endl;
 	cout.flush();
 	Mangrove_debug(ds);
-	cout<<"\tConverting the nested deque 'ds' into the corresponding nesting deque, containing several deques, ... ";
+	cout<<"\tThe total number of the 'uint' C++ built-in values in the new nested deque 'ds' is "<<Mangrove_valuesNumber(ds)<<"."<<endl<<endl;
+	cout<<"\tConverting the nested deque 'ds' back to the corresponding nesting deque, containing several deques, ... ";
 	Mangrove_convert(ds,d1);
-	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe total number of the 'uint' values in the new nested deque: "<<Mangrove_valuesNumber(d1)<<endl;
-	Mangrove_debug(d1);
+	cout<<"ok"<<endl;
+	if(Mangrove_theSame(d,d1)) cout<<"\tThe content of the new nested deque coincides with the content of the original nested deque 'd'."<<endl<<endl;
+	else cout<<"\tThe content of the new nested deque does not coincide with the content of the original nested deque 'd'."<<endl<<endl;
+	cout.flush();
+	
+	/* TASK #3 - deallocating all nested deques in this tutorial. */
 	cout<<"\tDeallocating all nested deques of this tutorial ... ";
 	Mangrove_destroy(d);
 	Mangrove_destroy(d1);
