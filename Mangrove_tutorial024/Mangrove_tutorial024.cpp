e1f1c19adf75ae231693ee03be19a3a2478475e2
David Canino <canino.david@gmail.com>
2017-03-19 01:01:50 +0100

    Minor revisions and updates to several tutorials

--- Mangrove_tutorial024/Mangrove_tutorial024.cpp
+++ Mangrove_tutorial024/Mangrove_tutorial024.cpp
@@ -18,7 +18,7 @@
  *
  * Mangrove_tutorial024.cpp - the 'Tutorial Example-024' for the 'Mangrove TDS Library 3.0'.
  *************************************************************************************************************************************************************************/
- 
+
 #include "Mangrove_Miscellanea.h"
 #include <cstdlib>
 #include <iostream>
@@ -32,68 +32,75 @@ using namespace mangrove_tds;
  * \author <A href="http://davidcanino.github.io">David Canino</A> (e-mail: <A href="mailto:canino.david@gmail.com">David Canino</A>)
  */
 
-/// The <i>main function</i> for the <i>Mangrove_tutorial024</i> program.
+/// The <i>main function</i> for the <i>'Mangrove_tutorial024'</i> program.
 /**
  * \return <ul><li>the built-in <i>EXIT_SUCCESS</i> constant value (provided by the <i><A href="http://en.wikipedia.org/wiki/C%2B%2B_Standard_Library">Standard C++ Library</A></i>), if all is ok</li><li>the built-in <i>EXIT_FAILURE</i> constant value (provided by 
  * the <i><A href="http://en.wikipedia.org/wiki/C%2B%2B_Standard_Library">Standard C++ Library</A></i>), otherwise</li></ul>
- * \see mangrove_tds::MANGROVE_ENTITY_DIM, mangrove_tds::MANGROVE_ENTITY_ID, mangrove_tds::MANGROVE_TARTAGLIA_TRIANGLE, mangrove_tds::Mangrove_exportCopyrightDisclaimer(), mangrove_tds::Mangrove_pause(), mangrove_tds::Mangrove_removeFile(), 
- * mangrove_tds::Mangrove_computeTartagliaTriangle(), mangrove_tds::Mangrove_destroyLocation4TartagliaTriangle(), mangrove_tds::Mangrove_destroyLocations4TartagliaTriangles(), mangrove_tds::Mangrove_getLocationsNumber4TartagliaTriangles(), 
- * mangrove_tds::Mangrove_debugTartagliaTriangles(), mangrove_tds::Mangrove_exportTartagliaTriangles(), mangrove_tds::Mangrove_reconstructTartagliaTriangles(), mangrove_tds::Mangrove_SerializableObject
+ * \see mangrove_tds::MANGROVE_ENTITY_DIM, mangrove_tds::MANGROVE_ENTITY_ID, mangrove_tds::MANGROVE_TARTAGLIA_TRIANGLE, mangrove_tds::Mangrove_exportCopyrightDisclaimer(), mangrove_tds::Mangrove_removeFile(), mangrove_tds::Mangrove_pause(),
+ * mangrove_tds::Mangrove_computeTartagliaTriangle(), mangrove_tds::Mangrove_getLocationsNumber4TartagliaTriangles(), mangrove_tds::Mangrove_destroyLocation4TartagliaTriangle(), mangrove_tds::Mangrove_destroyLocations4TartagliaTriangles(),
+ * mangrove_tds::Mangrove_debugTartagliaTriangles(), mangrove_tds::Mangrove_exportTartagliaTriangles(), mangrove_tds::Mangrove_reconstructTartagliaTriangles()
  */
 int main(void)
 {
-	MANGROVE_ENTITY_DIM d=4;
+	MANGROVE_ENTITY_DIM d=5;
 	uint pos;
 	ofstream out;
 	ifstream in;
 
-	/* First, we export the header for the tutorial. Then, we validate the analysis of the Tartaglia's Triangles. */
-	Mangrove_exportCopyrightDisclaimer("Mangrove_tutorial024");
+	/* This is the 'Mangrove_tutorial024' tutorial, where the Tartaglia's Triangles and the functions for their manipulation are validated. */
+	Mangrove_exportCopyrightDisclaimer ("The 'Mangrove_tutorial024' Tutorial");
+	cout.flush();
+	
+	/* TASK #1 - requesting a location for computing a new Tartaglia's Triangle, and exporting its 'debug' representation on the standard output stream. */
 	cout<<"\tRequesting a location in the 'tartaglia_triangles' deque for constructing the Tartaglia's Triangle of order "<<(uint)d<<" ... ";
 	Mangrove_computeTartagliaTriangle(d,pos);
-	cout<<"ok"<<endl<<endl;
-	cout<<"\tThe location #"<<pos<<" in the 'tartaglia_triangles' deque is assigned"<<endl;
-	cout<<"\tThe number of the locations in the 'tartaglia_triangles' deque is: "<<Mangrove_getLocationsNumber4TartagliaTriangles()<<endl<<endl;
-	cout<<"\tThe 'debug' representation for the location of interest in the 'tartaglia_triangles' deque is:"<<endl;
+	cout<<"ok"<<endl;
+	cout<<"\tThe location #"<<pos<<" in the 'tartaglia_triangles' deque is assigned for the new Tartaglia's Triangle."<<endl;
 	Mangrove_debugTartagliaTriangle(pos);
-	cout<<"\tExporting the 'compact' representation for the location of interest in the 'tartaglia_triangles' deque on the 'compact_tartaglia_triangle.txt' file ... ";
+	cout<<"\t#locations in the 'tartaglia_triangles' deque is "<<Mangrove_getLocationsNumber4TartagliaTriangles()<<"."<<endl<<endl;
+	cout.flush();
+	
+	/* TASK #2 - validating the serialization operators/functions on the Tartaglia's Triangles. */
+	cout<<"\tExporting the 'compact' representation for the location #"<<pos<<" in the 'tartaglia_triangles' deque on the 'compact_tartaglia_triangle.txt' file ... ";
 	out.open("compact_tartaglia_triangle.txt");
 	out<<tartaglia_triangles[pos];
 	out.close();
 	cout<<"ok"<<endl;
-	cout<<"\tDeallocating the location in position "<<pos<<" in the 'tartaglia_triangles' deque ... ";
+	cout<<"\tDeallocating the location #"<<pos<<" in the 'tartaglia_triangles' deque ... ";
 	Mangrove_destroyLocation4TartagliaTriangle(pos);
 	cout<<"ok"<<endl;
 	Mangrove_debugTartagliaTriangle(tartaglia_triangles[pos]);
-	cout<<"\tThe number of the locations in the 'tartaglia_triangles' deque is: "<<Mangrove_getLocationsNumber4TartagliaTriangles()<<endl<<endl;
-	cout<<"\tReconstructing the Tartaglia's Triangle of interest by reading its 'compact' representation from the 'compact_tartaglia_triangle.txt' file ... ";
+	cout<<"\t#locations in the 'tartaglia_triangles' deque is "<<Mangrove_getLocationsNumber4TartagliaTriangles()<<"."<<endl;
+	cout<<"\tReconstructing the location #"<<pos<<" in the 'tartaglia_triangles' deque by reading and parsing its 'compact' representation from the 'compact_tartaglia_triangle.txt' file ... ";
 	in.open("compact_tartaglia_triangle.txt");
 	in>>tartaglia_triangles[pos];
 	in.close();
 	cout<<"ok"<<endl;
 	Mangrove_debugTartagliaTriangle(pos);
-	cout<<"\tThe number of the locations in the 'tartaglia_triangles' deque is: "<<Mangrove_getLocationsNumber4TartagliaTriangles()<<endl<<endl;
+	cout<<"\t#locations in the 'tartaglia_triangles' deque is "<<Mangrove_getLocationsNumber4TartagliaTriangles()<<"."<<endl<<endl;
 	cout<<"\tRemoving the 'compact_tartaglia_triangle.txt' file ... ";
 	Mangrove_removeFile("compact_tartaglia_triangle.txt");
 	cout<<"ok"<<endl<<endl;
 	Mangrove_pause();
 	
-	/* Creating other Tartaglia's Triangles. */
+	/* TASK #3 - validating other operations on the Tartaglia's Triangles. */
 	for(MANGROVE_ENTITY_DIM d=2;d<5;d++)
 	{
 		cout<<"\tRequesting a location in the 'tartaglia_triangles' deque for constructing the Tartaglia's Triangle of order "<<(uint)d<<" ... ";
 		Mangrove_computeTartagliaTriangle(d,pos);
-		cout<<"ok. The location #"<<pos<<" in the 'tartaglia_triangles' deque is assigned."<<endl;
+		cout<<"ok"<<endl;
+		cout<<"\tThe location #"<<pos<<" in the 'tartaglia_triangles' deque is assigned for the new Tartaglia's Triangle."<<endl<<endl;
+		cout.flush();
 	}
 	
-	/* Now, we deallocate a location from the 'tartaglia_triangles' deque. */
-	cout<<endl<<"\tThe number of the locations in the 'tartaglia_triangles' deque is: "<<Mangrove_getLocationsNumber4TartagliaTriangles()<<endl;
-	cout<<"\tDeallocating the location in position 2 in the 'tartaglia_triangles' deque ... ";
-	Mangrove_destroyLocation4TartagliaTriangle(2);
-	cout<<"ok"<<endl;
-	cout<<"\tThe number of the locations in the 'tartaglia_triangles' deque is: "<<Mangrove_getLocationsNumber4TartagliaTriangles()<<endl<<endl;
+	/* Now, we export the 'debug' representation of all locations in the 'tartaglia_triangles' deque on the standard output stream. */
 	Mangrove_debugTartagliaTriangles();
-	cout<<"\tExporting the 'compact' representation of all locations in the 'tartaglia_triangles' deque on the 'compact_tartaglia_triangles.txt' file ... ";
+	pos=2;
+	cout<<"\tDeallocating the location #"<<pos<<" in the 'tartaglia_triangles' deque ... ";
+	Mangrove_destroyLocation4TartagliaTriangle(pos);
+	cout<<"ok"<<endl;
+	Mangrove_debugTartagliaTriangles();
+	cout<<"\tExporting the 'compact' representations of all locations in the 'tartaglia_triangles' deque on the 'compact_tartaglia_triangles.txt' file ... ";
 	out.open("compact_tartaglia_triangles.txt");
 	Mangrove_exportTartagliaTriangles(out);
 	out.close();
@@ -101,8 +108,8 @@ int main(void)
 	cout<<"\tDeallocating all Tartaglia's Triangles in the 'tartaglia_triangles' deque ... ";
 	Mangrove_destroyLocations4TartagliaTriangles();
 	cout<<"ok"<<endl;
-	cout<<"\tThe number of the locations in the 'tartaglia_triangles' deque is: "<<Mangrove_getLocationsNumber4TartagliaTriangles()<<endl;
-	cout<<"\tReconstructing all Tartaglia's Triangles in the 'tartaglia_triangles' deque by reading its 'compact' representation from the 'compact_tartaglia_triangles.txt' file ... ";
+	Mangrove_debugTartagliaTriangles();
+	cout<<"\tReconstructing all Tartaglia's Triangles in the 'tartaglia_triangles' deque by reading and parsing their 'compact' representations from the 'compact_tartaglia_triangles.txt' file ... ";
 	in.open("compact_tartaglia_triangles.txt");
 	Mangrove_reconstructTartagliaTriangles(in);
 	in.close();
@@ -114,6 +121,7 @@ int main(void)
 	cout<<"\tDeallocating all Tartaglia's Triangles in the 'tartaglia_triangles' deque ... ";
 	Mangrove_destroyLocations4TartagliaTriangles();
 	cout<<"ok"<<endl;
-	cout<<"\tThe number of the locations in the 'tartaglia_triangles' deque is: "<<Mangrove_getLocationsNumber4TartagliaTriangles()<<endl<<endl;
-	return EXIT_SUCCESS;
+	cout<<"\t#locations in the 'tartaglia_triangles' deque is "<<Mangrove_getLocationsNumber4TartagliaTriangles()<<"."<<endl<<endl;
+	cout.flush();
+	return EXIT_SUCCESS;	
 }
